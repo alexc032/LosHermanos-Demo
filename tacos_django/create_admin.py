37b0010ea147587cff6aa2bbe91139082d562ade
alejandromennedez-boop <alejandromennedez@gmail.com>
2026-04-17 20:07:52 -0600

    Update admin credentials script

--- tacos_django/create_admin.py
+++ tacos_django/create_admin.py
@@ -6,8 +6,8 @@ django.setup()
 
 from django.contrib.auth.models import User
 
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@elsabormexicano.com', 'Tacos123!')
-    print("Superusuario 'admin' creado con contraseña 'Tacos123!'")
+if not User.objects.filter(username='ElHermano_228391').exists():
+    User.objects.create_superuser('ElHermano_228391', 'ElHermano_228391@gmail.com', 'H3rmanosTacos$2149!')
+    print("Superusuario 'ElHermano_228391' creado con contraseña 'H3rmanosTacos$2149!'")
 else:
     print("El superusuario ya existe.")
